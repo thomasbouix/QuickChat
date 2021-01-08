7cfaf5b22b64f475963a7ec3bb15c8936bb174b8
Melissa <lebretonmelissa.s@gmail.com>
2021-01-08 23:09:20 +0100

    Renommage branche

--- quick_tools.py
+++ quick_tools.py
@@ -1,7 +1,14 @@
 import sqlite3
 
 def createDb(db_path):
+	 connect = sqlite3.connect(db_path)
 
+     cursor = connect.cursor()
+
+     cursor.execute('CREATE TABLE Room ([id] INTEGER PRIMARY KEY,[name] TEXT UNIQUE, [password] TEXT NOT NULL, [private] BOOLEAN, [size] INTEGER)')
+     cursor.execute('CREATE TABLE User ([id] INTEGER PRIMARY KEY,[username] TEXT UNIQUE, [password] TEXT NOT NULL)')
+     cursor.execute('CREATE TABLE Message ([id] INTEGER PRIMARY KEY,[userId] INTEGER NOT NULL, [roomId] INTEGER NOT NULL, [mess] TEXT NOT NULL, [sendDate] TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(userId) REFERENCES User(id), FOREIGN KEY(roomId) REFERENCES Room(id))')
+     connect.commit()
 
 # Db creation :
 db_path = 'quick_chat.db'
