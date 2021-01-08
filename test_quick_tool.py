84c608beefc061bdf9a927ae05037a79c6fa7f38
diegopanepo <diegomv1997@hotmail.com>
2021-01-08 18:34:59 +0100

    test creation table

--- test_quick_tool.py
+++ test_quick_tool.py
@@ -1,6 +1,15 @@
 import unittest
+import sqlite3
 from quick_tools import createDb
+import random
+import string
 
 class QuickToolsTester(unittest.TestCase):
 
-    def test_createDb(self):
+	def test_verif_creation_tables(self):
+		random_len = random.randint(1,20)
+		test_nom = ''.join(random.choice(string.ascii_letters) for i in range(random_len))
+		self.assertFalse(verif_creation_tables(test_nom))
+		self.assertTrue(verif_creation_tables('Room'))
+		self.assertTrue(verif_creation_tables('Message'))
+		self.assertTrue(verif_creation_tables('User'))
