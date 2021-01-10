bf5985cbdfae6c3cf183718b33d91f314d07ca6d
Melissa <lebretonmelissa.s@gmail.com>
2021-01-10 13:04:54 +0100

    Ajout deleteDb + test

--- QuickChat_bdd.py
+++ QuickChat_bdd.py
@@ -7,12 +7,18 @@ def createDb(db_path):
 	cursor.execute('CREATE TABLE Room ([id] INTEGER PRIMARY KEY,[name] TEXT UNIQUE, [password] TEXT NOT NULL, [private] BOOLEAN, [size] INTEGER)')
 	cursor.execute('CREATE TABLE User ([id] INTEGER PRIMARY KEY,[username] TEXT UNIQUE, [password] TEXT NOT NULL)')
 	cursor.execute('CREATE TABLE Message ([id] INTEGER PRIMARY KEY,[userId] INTEGER NOT NULL, [roomId] INTEGER NOT NULL, [mess] TEXT NOT NULL, [sendDate] TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(userId) REFERENCES User(id), FOREIGN KEY(roomId) REFERENCES Room(id))')
+
 	connect.commit()
 
-# def verifCreationTables(table):
-# 	if table == 'Room' or table == 'User' or table == 'Message':
-# 		return True
-# 	return False
+def deleteDb(db_path):
+	connect = sqlite3.connect(db_path)
+	cursor = connect.cursor()
+
+	cursor.execute('DROP TABLE IF EXISTS Room')
+	cursor.execute('DROP TABLE IF EXISTS User')
+	cursor.execute('DROP TABLE IF EXISTS Message')
+
+	connect.commit()
 
 # Db creation :
 db_path = 'quick_chat.db'
