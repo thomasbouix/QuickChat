09735f7a0ebfe770932a54396b14cfcdc4211d5e
Melissa <lebretonmelissa.s@gmail.com>
2021-01-13 18:54:18 +0100

    Correction des tests

--- tests/testBdd.py
+++ tests/testBdd.py
@@ -1,8 +1,8 @@
-import unittest
-import sqlite3
-from QuickChat_bdd import deleteDb
+import unittest, sqlite3, sys
+sys.path[:0] = ['../']
+import QuickChat_bdd
 from QuickChat_bdd import createDb
-
+from QuickChat_bdd import deleteDb
 
 class test_creation_delete_bdd(unittest.TestCase):
 
@@ -12,17 +12,22 @@ class test_creation_delete_bdd(unittest.TestCase):
 		self.connect = sqlite3.connect(self.db_path)
 		self.cursor = self.connect.cursor()
 
+#sqlite_sequence : table interne qui gère les AUTOINCREMENT + insupprimable
+
 	def test_createDb(self):
 		createDb(self.db_path)
 		sql = "SELECT name FROM sqlite_master WHERE type='table';"
-		#print(self.cursor.execute(sql).fetchall())
-		self.assertEqual(self.cursor.execute(sql).fetchall(), [('Room',), ('User',), ('Message',)])
+		res = self.cursor.execute(sql).fetchall()
+		self.assertIn(('Room',), res)
+		self.assertIn(('User',), res)
+		self.assertIn(('Message',), res)
+		self.assertIn(('sqlite_sequence',), res)
 
 	def test_deleteDb(self):
 		deleteDb(self.db_path)
 		sql = "SELECT name FROM sqlite_master WHERE type='table';"
 		#print(self.cursor.execute(sql).fetchall())
-		self.assertEqual(self.cursor.execute(sql).fetchall(), [])
+		self.assertEqual(self.cursor.execute(sql).fetchall(), [('sqlite_sequence',)])
 
 if __name__ == '__main__':
     unittest.main()
