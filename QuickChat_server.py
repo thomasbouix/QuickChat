bd4e8bfb00d449873d31687a82fbc0c4eb6933fe
Theo <theo.germain@icloud.com>
2021-01-19 17:13:49 +0100

    [US-142] Ajout d'une fonction qui prend le nom d'une room et retourne une liste de messages mis en forme.

--- QuickChat_server.py
+++ QuickChat_server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room, leave_room
+from datetime import datetime
+from QuickChat_bdd import *
 
 # for socketio
 import eventlet
@@ -8,6 +10,17 @@ eventlet.monkey_patch()
 app = Flask(__name__)
 app.config['TESTING'] = True
 socketio = SocketIO(app, async_mode='eventlet')
+
+
+def getHistorique(roomName):
+    historique = []
+    roomId = getRoomId(roomName)
+    messages = getMessagesByRoomId(roomId)
+    for message in messages:
+        historique.append('{} - {} : {}'.format(message[4].split('.')[0], getUsernameById(message[1]), message[3]))
+
+    return historique
+
 
 def main():
     socketio.run(app)
