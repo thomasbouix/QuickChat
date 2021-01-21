5f72aaf731c598b0d30c181a30bc55ddc2654e53
BaoHuiling <56514022+BaoHuiling@users.noreply.github.com>
2021-01-21 21:41:31 +0100

    [US232] Integre add_user dans bdd

--- QuickChat_bdd.py
+++ QuickChat_bdd.py
@@ -48,6 +48,42 @@ def getRoomId(roomName):
 	roomId = cursor.execute(sql).fetchone()[0]
 
 	return roomId
+ import sqlite3
+# from QuickChat_bdd import CreateDb
+
+
+def verifyUserPassword(user_password):
+	# Extra requirement: check the password have number,special character, length>8 
+	is_number = 0
+	special_character = 0
+
+	for i in user_password:
+		if i.isdigit():
+			is_number = 1
+
+		if (not i.islower()) and (not i.isupper())  and (not i.isdigit()):
+			special_character = 1
+
+	if len(user_password)>=8:
+		if is_number and special_character:
+			return True
+		
+	return False
+
+def addUser(db_path, username, password):
+
+	connect = sqlite3.connect(db_path)
+	cursor = connect.cursor()
+	
+	
+	if verifyUserPassword(password):
+		sql = 'INSERT INTO User (username, password) VALUES (?,?)'
+		cursor.execute(sql,(username, password))
+
+	else:
+		print("\nUsers ERROR: password should > 8 chars, includes numbers and special character")
+	
+	connect.commit()
 
 # Db creation :
 db_path = 'quick_chat.db'
