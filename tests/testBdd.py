4d1189e62b8ced3b7a77ffdc75f9d2e36d78b95e
BaoHuiling <56514022+BaoHuiling@users.noreply.github.com>
2021-01-21 21:49:15 +0100

    [US232] integre test_user dans test_bdd

--- tests/testBdd.py
+++ tests/testBdd.py
@@ -74,6 +74,39 @@ class testBDD(unittest.TestCase):
 		res = QuickChat_bdd.getRoomId(roomName)
 		# print(string)
 		self.assertEqual(self.cursor.execute(sql).fetchall()[0][0], res)
+		
+	def test_verifyUserPassword(self):
+
+		self.assertFalse(QuickChat_bdd.verifyUserPassword('qwer')) # not long enough
+		self.assertFalse(QuickChat_bdd.verifyUserPassword('qwer123456')) # no special character
+
+		random_str_len = random.randint(5,10)
+		
+		correct_password = ''.join(random.choice(string.ascii_lowercase) for i in range(random_str_len))
+		correct_password += '123456,'
+
+		self.assertTrue(QuickChat_bdd.verifyUserPassword(correct_password))
+
+	def test_addUser(self):
+
+		self.cursor.execute('DROP TABLE IF EXISTS Room;')
+		self.cursor.execute('DROP TABLE IF EXISTS User;')
+		self.cursor.execute('DROP TABLE IF EXISTS Message;')
+		QuickChat_bdd.createDb(db_path)
+
+		QuickChat_bdd.addUser(db_path,'yann.c','qwer123456,')  # add a correct user
+		sql = "select username from User where username = 'yann.c';"
+		user_name = ''
+		for row in self.cursor.execute(sql):
+			user_name = row[0]
+		self.assertEqual(user_name,'yann.c')
+
+		QuickChat_bdd.addUser(db_path,'huiling.b','pass')  # add a user with wrong password format
+		sql = "select username from User where username = 'huiling.b';"
+		name = ''
+		for row in  self.cursor.execute(sql):
+			name = row[0]
+		self.assertEqual(name,'')
 
 
 if __name__ == '__main__':
