e37042a6b3c0466e6f6e7b739a027d51891f9d0b
lephoquebleu <younes.chefou@gmail.com>
2021-02-02 22:24:28 +0100

    [US15] Test autonome avec subprocess, copié dans testServer.py

--- tests/testServer.py
+++ tests/testServer.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 
 import unittest, os, sys, unittest
+
+import socketio, time, sqlite3
+
+import shutil,shlex, subprocess
+
 sys.path[:0] = ['../']
-import QuickChat_server, QuickChat_client, QuickChat_bdd, sqlite3
-from datetime import *
+import QuickChat_client
+import QuickChat_server as server
+import QuickChat_bdd as bdd
+from datetime import datetime
 
 class testServer(unittest.TestCase):
 
     db_path = 'quick_chat.db'
+    list_subprocess = []
 
-    def test_reception_historique(self): 
+    def test_reception_historique(self):
         date = datetime.now()
-        QuickChat_bdd.resetDb(self.db_path)
+        bdd.resetDb(self.db_path)
         connect = sqlite3.connect(self.db_path)
 
         cursor = connect.cursor()
@@ -20,9 +28,129 @@ class testServer(unittest.TestCase):
         cursor.execute('INSERT INTO Message (userId, roomId, mess, sendDate) VALUES (1,1,"Mon premier message","{}")'.format(date))
         connect.commit()
 
-        res = QuickChat_server.getHistorique("room1")
+        res = server.getHistorique("room1")
         self.assertTrue( res == ['{} - user1 : Mon premier message'.format(str(date).split('.')[0])])
 
+    def kill_subprocess(self):
+        while len(self.list_subprocess) != 0 :
+            p = self.list_subprocess.pop()
+            p.terminate()
+
+    def launch_server(self):
+
+        cmd = "python3.7 ../QuickChat_server.py &"
+        args = shlex.split(cmd)
+        p  = subprocess.Popen(args) # launch command as a subprocess
+        self.list_subprocess.append(p)
+        time.sleep(5) #Temps que le serveur se mette en place
+
+    def setUp(self):
+
+        self.launch_server()
+
+        self.db_path = 'quick_chat.db'
+        self.conn = sqlite3.connect(self.db_path)
+        self.cursor = self.conn.cursor()
+        self.sio_test = socketio.Client()
+
+        #Affiche un message lorsque le serveur nous informe que le
+        #client est connecté
+        @self.sio_test.on('connect')
+        def connect():
+            print("Connecté au serveur.")
+            print(self.sio_test.get_sid())
+            # self.sio_test.emit('message', {"data" : "Message test"})
+
+        #Affiche les messages envoyés par le serveur
+        @self.sio_test.on('message')
+        def message(data):
+            print(data)
+
+        self.sio_test.connect('http://localhost:5000')
+
+        #Création de la BDD
+        bdd.createDb(self.db_path)
+        req = "INSERT INTO ROOM(name, password, private, size) VALUES(\"room_test\", \"\", 1, 10);"
+        self.cursor.execute(req)
+        self.conn.commit()
+
+        time.sleep(2)
+
+    def tearDown(self):
+        bdd.deleteDb(self.db_path)
+        self.sio_test.disconnect()
+        self.kill_subprocess()
+
+    def test_reception_donnees_connexion(self):
+
+        #On emet des données type connexion au serveur
+        self.sio_test.emit('connexion', {"username":"Jean", "room":"room_test"})
+        self.sio_test.emit('connexion', {"username":"Jeremy", "room":"room_test"})
+        self.sio_test.emit('connexion', {"username":"Jonathan", "room":"room_test"})
+
+        #On fait attendre le test 2 secondes afin que l'ajout des données ait le
+        #temps de se faire
+        time.sleep(2)
+
+        #Verification de l'ajout du user dans la BDD
+        req = "SELECT username FROM USER;"
+        res = self.cursor.execute(req).fetchall()
+        self.conn.commit()
+        # print(res)
+        self.assertEqual(res, [('Jean',), ('Jeremy',), ('Jonathan',)])
+
+
+    def test_reception_donnees_message(self):
+
+        #On emet des données type connexion au serveur
+        self.sio_test.emit('connexion', {"username":"Jean", "room":"room_test"})
+        self.sio_test.emit('connexion', {"username":"Jeremy", "room":"room_test"})
+        self.sio_test.emit('connexion', {"username":"Jonathan", "room":"room_test"})
+
+        #On emet des données type message au serveur
+        self.sio_test.emit('message_user',\
+         {"username":"Jean",\
+         "message":"Bonjour, je suis Jean"})
+        self.sio_test.emit('message_user',\
+         {"username":"Jeremy",\
+          "message":"Bonjour, je suis Jeremy"})
+        self.sio_test.emit('message_user',\
+         {"username":"Jonathan",\
+          "message":"Bonjour, je suis Jonathan"})
+        self.sio_test.emit('message_user',\
+         {"username":"Jeremy",\
+          "message":"Bonjour tout le monde !"})
+
+        #On fait attendre le test 2 secondes afin que l'ajout des données ait le
+        #temps de se faire
+        time.sleep(2)
+
+        req = "SELECT id, userId, roomId, mess FROM MESSAGE WHERE id=1;"
+        res = self.cursor.execute(req).fetchall()
+        self.conn.commit()
+        # print(res)
+        self.assertEqual(res, [(1, 1, 1, 'Bonjour, je suis Jean')])
+
+        req = "SELECT id, userId, roomId, mess FROM MESSAGE WHERE id=2;"
+        res = self.cursor.execute(req).fetchall()
+        self.conn.commit()
+        # print(res)
+        self.assertEqual(res, [(2, 2, 1, 'Bonjour, je suis Jeremy')])
+
+
+        req = "SELECT id, userId, roomId, mess FROM MESSAGE WHERE id=3;"
+        res = self.cursor.execute(req).fetchall()
+        self.conn.commit()
+        # print(res)
+        self.assertEqual(res, [(3, 3, 1, 'Bonjour, je suis Jonathan')])
+
+
+        req = "SELECT id, userId, roomId, mess FROM MESSAGE WHERE id=4;"
+        res = self.cursor.execute(req).fetchall()
+        self.conn.commit()
+        # print(res)
+        self.assertEqual(res, [(4, 2, 1, 'Bonjour tout le monde !')])
+        # self.kill_subprocess()
 
 if __name__ == '__main__':
     unittest.main()
