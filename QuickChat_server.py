6823693680d965f940594068368435c201091dc5
BaoHuiling <baozii.721@gmail.com>
2021-02-09 15:47:49 +0100

    [US16] tab to blankspace

--- QuickChat_server.py
+++ QuickChat_server.py
@@ -100,48 +100,48 @@ def getHistorique(roomName):
 
 @socketio.on('Signup_room')
 def add_room(roomInfo):
-	addRoom(roomInfo['roomname'], roomInfo['password'], roomInfo['private'], roomInfo['size'])
+    addRoom(roomInfo['roomname'], roomInfo['password'], roomInfo['private'], roomInfo['size'])
 
 
 @socketio.on('Signup_user')
 def add_user(data):
-	addUser(db_path,data['username'],data['password'])
+    addUser(db_path,data['username'],data['password'])
 
 @socketio.on('message')
 def handle_message(data):
-	username = data['username']
-	payload = data['payload']
-	addmessagefromclient(username,payload)
-	print('received message from {}: '.format(data['username']) + data['payload'])
-	# socketio.emit('message', data, broadcast=True, room=data['room'])
+    username = data['username']
+    payload = data['payload']
+    addmessagefromclient(username,payload)
+    print('received message from {}: '.format(data['username']) + data['payload'])
+    # socketio.emit('message', data, broadcast=True, room=data['room'])
 
 @socketio.on('join')
 def on_join(data):
-	username = data['username']
-	room = data['room']
-	join_room(room,username)
+    username = data['username']
+    room = data['room']
+    join_room(room,username)
 
 def join_room(room,username):
-	iduser = getIDfromusername(username)
-	idroom = getRoomId(db_path,room)
-	if iduser == 0:
-		print('\033[91mServer log\033[0m Please sign up with this username')
-		# emit('message', {'username': 'server' ,'payload': '\033[94m{} is not declared.\033[0m'.format(username)}, room=room)
-	elif idroom == 0:
-		print('\033[91mServer log\033[0m Please sign up with this roomname')
-		# emit('message', {'username': 'server' ,'payload': '\033[94m{} is not declared.\033[0m'.format(room)}, room=room)
-	else:
-		join_roomfromid(iduser,idroom)
-		print('\033[91mServer log\033[0m {} has joined {}'.format(username, room))
-		# emit('message', {'username': 'server' ,'payload': '\033[94m{} has entered the room.\033[0m'.format(username)}, room=room)
+    iduser = getIDfromusername(username)
+    idroom = getRoomId(db_path,room)
+    if iduser == 0:
+        print('\033[91mServer log\033[0m Please sign up with this username')
+        # emit('message', {'username': 'server' ,'payload': '\033[94m{} is not declared.\033[0m'.format(username)}, room=room)
+    elif idroom == 0:
+        print('\033[91mServer log\033[0m Please sign up with this roomname')
+        # emit('message', {'username': 'server' ,'payload': '\033[94m{} is not declared.\033[0m'.format(room)}, room=room)
+    else:
+        join_roomfromid(iduser,idroom)
+        print('\033[91mServer log\033[0m {} has joined {}'.format(username, room))
+        # emit('message', {'username': 'server' ,'payload': '\033[94m{} has entered the room.\033[0m'.format(username)}, room=room)
 
 @socketio.on('leave')
 def on_leave(data):
-	username = data['username']
-	room = data['room']
-	leave_room(room,username)
-	print('\033[91mServer log\033[0m {} has left {}'.format(data['username'], data['room']))
-	# emit('message', {'username': 'server', 'payload': '\033[94m{} has left the room.\033[0m'}.format(username), room=room)
+    username = data['username']
+    room = data['room']
+    leave_room(room,username)
+    print('\033[91mServer log\033[0m {} has left {}'.format(data['username'], data['room']))
+    # emit('message', {'username': 'server', 'payload': '\033[94m{} has left the room.\033[0m'}.format(username), room=room)
 
 # private = 1: private, private = 0: public
 def addRoom(name, password, private, size):
