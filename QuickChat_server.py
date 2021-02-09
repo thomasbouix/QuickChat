8ff2dbb3fe2ca9403d6fd8fe1300405805a38c0b
ZekAxel <axel@arctique.fr>
2021-02-09 09:43:33 +0100

    Résolution conflits Quickchat_server

--- QuickChat_server.py
+++ QuickChat_server.py
@@ -12,30 +12,30 @@ app.config['TESTING'] = True
 socketio = SocketIO(app, async_mode='eventlet')
 
 def createTableRoom():
-    	conn = sqlite3.connect('quick_chat.db')
-	c = conn.cursor()
+		conn = sqlite3.connect('quick_chat.db')
+		c = conn.cursor()
 
-	# Create the table Room
-	req = '''CREATE TABLE Room(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT NOT NULL,
-		password TEXT NOT NULL,
-		private BOOLEAN NOT NULL,
-		size INTEGER NOT NULL 
-	);'''
-	c.execute(req)
-	conn.commit()
+		# Create the table Room
+		req = '''CREATE TABLE Room(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			password TEXT NOT NULL,
+			private BOOLEAN NOT NULL,
+			size INTEGER NOT NULL 
+		);'''
+		c.execute(req)
+		conn.commit()
 
 
 # private = 1: private, private = 0: public
 def addRoom(name, password, private, size):
-	conn = sqlite3.connect('quick_chat.db')
-	c = conn.cursor()
+		conn = sqlite3.connect('quick_chat.db')
+		c = conn.cursor()
 
-	# Insert a new room in table
-	req = '''INSERT INTO Room (name, password, private, size) VALUES ("%s", "%s", %d, %d);''' % (name, password, private, size)
-	c.execute(req)
-	conn.commit()
+		# Insert a new room in table
+		req = '''INSERT INTO Room (name, password, private, size) VALUES ("%s", "%s", %d, %d);''' % (name, password, private, size)
+		c.execute(req)
+		conn.commit()
 
 def main():
     socketio.run(app)
