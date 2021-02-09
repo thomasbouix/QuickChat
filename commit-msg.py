242be19e441a072bec7eb02414df1140649673b0
jufray <juliefraysse@free.fr>
2021-02-09 16:29:32 +0100

    [US9393] kdk ojejf ofeo

--- commit-msg.py
+++ commit-msg.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 
 import sys
+import re
 
 def main():
+    """ Description : verification du format du nom du commit """
     print("Number of arguments: %d" % len(sys.argv))
     print("Argument List: %s" % str(sys.argv))
+    with open(sys.argv[1], "r") as fp:
+        ligne = fp.readlines()
 
+    message = str(ligne[0]).rstrip()
+    print(message)
+
+    pattern = re.compile("\[(US|DO)([1-9]+)\](\s{1})([a-z]{1,}|\s)*")
+    result = pattern.match(message)
+    if result == None :
+        print("Le format du commit n'est pas valide.\nExemple de format valide : \"[US100] mon message\"")
+        sys.exit(1)
     sys.exit(0)
 
 if __name__ == "__main__":
